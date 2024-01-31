6d213a77fa0396c9bee616cc302444a4c6dcce73
naka-nana <harunanapanda@icloud.com>
2024-01-31 16:17:40 +0900

    デプロイ修正

--- db/migrate/20240130035914_add_user_id_to_prototypes.rb
+++
@@ -1,6 +0,0 @@
-class AddUserIdToPrototypes < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :prototypes, :user_id if column_exists?(:prototypes, :user_id)
-    add_column :prototypes, :user_id, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_30_035914) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_29_053731) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -40,12 +40,11 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_30_035914) do
   end
 
   create_table "comments", charset: "utf8", force: :cascade do |t|
+    t.text "content", null: false
     t.bigint "user_id", null: false
     t.bigint "prototype_id", null: false
-    t.text "text", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "content"
     t.index ["prototype_id"], name: "index_comments_on_prototype_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
@@ -54,18 +53,19 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_30_035914) do
     t.string "title", null: false
     t.text "catch_copy", null: false
     t.text "concept", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
+    t.index ["user_id"], name: "index_prototypes_on_user_id"
   end
 
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "name", default: "", null: false
-    t.string "profile", default: "", null: false
-    t.string "occupation", default: "", null: false
-    t.string "position", default: "", null: false
+    t.string "name", null: false
+    t.text "profile", null: false
+    t.text "occupation", null: false
+    t.text "position", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -77,4 +77,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_30_035914) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "comments", "prototypes"
+  add_foreign_key "comments", "users"
+  add_foreign_key "prototypes", "users"
 end
